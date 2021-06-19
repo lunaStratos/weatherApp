513c3a75d7272c49a3377791508f48cb9dd6dd1a
lostman <lunastratos@studiox.co.kr>
2021-06-19 17:29:09 +0900

    애니메이티드 날씨

--- lib/Screen/DecorationWidget/AnimatedImage.dart
+++ lib/Screen/DecorationWidget/AnimatedImage.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:rainvow_mobile/Util/Util.dart';
 
 
@@ -20,6 +23,7 @@ class _AnimatedImageState extends State<AnimatedImage>
 
   String weatherConditions = "0";
   _AnimatedImageState({required this.weatherConditions});
+  double opacityDouble = 0.0;
 
   late final AnimationController _controller = AnimationController(
     vsync: this,
@@ -39,15 +43,49 @@ class _AnimatedImageState extends State<AnimatedImage>
     super.dispose();
     _controller.dispose();
   }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(milliseconds: 200), () {
+      print("Yeah, this line is printed after 1 seconds");
+      setState(() {
+        opacityDouble = 1.0;
+      });
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         // Image.asset('assets/images/clouds.png'),
+        Container(
+          margin: EdgeInsets.only(left: 20, top: 100, right: 20, bottom: 50),
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 5), // changes position of shadow
+              ),
+            ],
+          ),
+        ),
         SlideTransition(
           position: _animation,
-          child: Image.asset('${Util.kmaNowImgAddress(weatherConditions)}'),
+          child: AnimatedOpacity(opacity: opacityDouble ,duration: Duration(seconds: 1),child: Image.asset('${Util.kmaNowImgAddress(weatherConditions)}'),),
         ),
       ],
     );
